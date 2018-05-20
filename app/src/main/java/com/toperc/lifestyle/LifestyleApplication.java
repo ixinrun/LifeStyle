04a76a985b8f28636c1bfb95abc3c0312015bd87
toperc <642545912@qq.com>
2018-05-20 15:40:46 +0800

    Screen adaptation

--- app/src/main/java/com/toperc/lifestyle/LifestyleApplication.java
+++ app/src/main/java/com/toperc/lifestyle/LifestyleApplication.java
@@ -31,18 +31,25 @@ public class LifestyleApplication extends Application {
         //绘制页面时参照的设计图尺寸
         final float DESIGN_WIDTH = 1080f;
         final float DESIGN_HEIGHT = 1920f;
-        final float DESTGN_INCH = 4.91f;
-        //获取参考密度值
-        float referenceDensity = (float) Math.sqrt(DESIGN_WIDTH * DESIGN_WIDTH + DESIGN_HEIGHT * DESIGN_HEIGHT) / DESTGN_INCH / 160;
-        //获取放大缩小比率
+        final float DESTGN_INCH = 5.0f;
+        //大屏调节因子，范围0~1，因同比例放大造成大屏幕视图显示非常傻大憨，用于调节感官度。
+        final float BIG_SCREEN_FACTOR = 0.2f;
+
         DisplayMetrics dm = getResources().getDisplayMetrics();
+        //确定放大缩小比率
         float rate = 1f;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             rate = dm.widthPixels / DESIGN_WIDTH;
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             rate = dm.widthPixels / DESIGN_HEIGHT;
         }
-        //重置系统密度值
-        dm.density = referenceDensity * rate;
+        //确定参照屏幕密度
+        float referenceDensity = (float) Math.sqrt(DESIGN_WIDTH * DESIGN_WIDTH + DESIGN_HEIGHT * DESIGN_HEIGHT) / DESTGN_INCH / 160;
+        //确定最终屏幕密度
+        float relativeDensity = referenceDensity * rate;
+        if (relativeDensity > dm.density) {
+            relativeDensity = relativeDensity - (relativeDensity - dm.density) * BIG_SCREEN_FACTOR;
+        }
+        dm.density = relativeDensity;
     }
 }
