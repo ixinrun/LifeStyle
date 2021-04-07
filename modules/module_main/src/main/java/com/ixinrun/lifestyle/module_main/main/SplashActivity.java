9c9d5d3b4a36ce4b6cae15622c1fd2d5644a516b
ixinrun <642545912@qq.com>
2021-04-07 17:11:17 +0800

    feat: 闪屏页面添加装饰

--- modules/module_main/src/main/java/com/ixinrun/lifestyle/module_main/main/SplashActivity.java
+++ modules/module_main/src/main/java/com/ixinrun/lifestyle/module_main/main/SplashActivity.java
@@ -3,7 +3,9 @@ package com.ixinrun.lifestyle.module_main.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 
 import com.ixinrun.lifestyle.common.base.BaseLsAct;
 import com.ixinrun.lifestyle.module_main.R;
@@ -11,14 +13,19 @@ import com.ixinrun.lifestyle.module_main.R;
 public class SplashActivity extends BaseLsAct {
 
     @Override
-    protected void initBase() {
-        super.initBase();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    protected boolean isTranslucentStatusBar() {
+        return true;
     }
 
     @Override
     protected void initView() {
         setContentView(R.layout.activity_splash);
+        View contentView = findViewById(R.id.content_ll);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.5f, 1f);
+        alphaAnimation.setInterpolator(new AccelerateInterpolator());
+        alphaAnimation.setDuration(1500);
+        contentView.startAnimation(alphaAnimation);
     }
 
     @Override
